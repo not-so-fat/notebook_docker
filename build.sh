c590e4865402918505d38ca64524a404029b10a8
Not So Fat <yusuke.muraoka.0208@gmail.com>
2020-09-29 13:55:11 -0700

    change default package

--- build.sh
+++ build.sh
@@ -2,5 +2,12 @@
 
 USERID=`id -u`
 PASSWORD=$1
+
+if [ $# -eq 0 ]
+  then
+    echo "No password supplied"
+    exit 1
+fi
+
 echo $USERID
 docker build -t my_notebook:0.0 --build-arg USERID=$USERID --build-arg PASSWORD=$PASSWORD ./
